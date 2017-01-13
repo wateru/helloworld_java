531721fb92234300071e4ad0838c5c3b90a6514e
parkminho <water4000@naver.com>
2017-01-13 12:43:19 +0900

    project properti 설정 etf로 바꿔 한글 다시쓰면 한글 윈도우서보임

--- Helloworld/src/com/test/helloworld/HelloWorld.java
+++ Helloworld/src/com/test/helloworld/HelloWorld.java
@@ -3,8 +3,7 @@ package com.test.helloworld;
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Heloow, World~!");
-		
-		dsgsdgsdg
+		System.out.println("안녕하세요");
 	}
 }
 
