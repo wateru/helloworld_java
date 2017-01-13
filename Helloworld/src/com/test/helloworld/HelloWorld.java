9fd73a12cb2d68b69ba25d2e03e93d4a25569358
parkminho <water4000@naver.com>
2017-01-13 14:12:09 +0900

    초기 버전

--- Helloworld/src/com/test/helloworld/HelloWorld.java
+++ Helloworld/src/com/test/helloworld/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Heloow, World~!");
 		System.out.println("안녕하세요");
+		
+	
 	}
 }
 
---
+++ VariableExample/src/com/test/variable_exam/VariableExam.java
@@ -0,0 +1,10 @@
+package com.test.variable_exam;
+
+public class VariableExam {
+	public static void main(String args[]){
+		int value = 10;
+		int result = value+10;
+		System.out.println(result);
+		
+	}
+}
